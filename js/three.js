86b647aa8eddeb773bf8275e6bbeb3b853bcc7bb
Your Name <tuur.clijsters@gmail.com>
2020-02-18 21:06:21 +0100

    bowling in andere DOM

--- js/three.js
+++ js/three.js
@@ -1,85 +1,105 @@
 var scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xff0000 );
+scene.background = new THREE.Color( 0xC0C0C0 );
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
-
-// camera.position.z = -501; 
-// camera.position.y = 15; 
-
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-var controls = new THREE.OrbitControls( camera, renderer.domElement );
-
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
 camera.position.z = 5;
 
-function animate() {
-    requestAnimationFrame( animate );
+// var renderer = new THREE.WebGLRenderer();
+// renderer.setSize( window.innerWidth, window.innerHeight );
 
-    controls.update();
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
-}
-animate();
+const canvas = document.getElementById(`canvas`);
+const renderer = new THREE.WebGLRenderer({canvas, antialias: false});
 
+// var main = document.getElementById('main');
+// document.body.appendChild( renderer.domElement );
 
 
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+// CUBE ANIMATION
+
+// var geometry = new THREE.BoxGeometry();
+// var material = new THREE.MeshLambertMaterial( {
+// color:  0x0000FF,
+// // transparent:true,
+// // opacity:1,
+// // wireframe:true
+// } );
+// var cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+// var ambientLight = new THREE.AmbientLight( 0xFFFFFF, 5.0);
+// scene.add(ambientLight);
+
+// function animate() {
+//     requestAnimationFrame( animate );
+
+//     controls.update();
+//     // cube.rotation.x += 0.01;
+//     // cube.rotation.y += 0.01;
+// 	renderer.render( scene, camera );
+// }
+// animate();
+
+
+// Instantiate a loader
 var loader = new THREE.GLTFLoader();
 
-var ship_material = new THREE.MeshBasicMaterial( { color: 0x444444 } );
-
+// Load a glTF resource
 loader.load(
-    // resource URL
+	// resource URL
+    // '/assets/ball/scene.gltf',
     '/assets/models/scene.gltf',
-    // called when the resource is loaded
+	// called when the resource is loaded
+	function ( gltf ) {
 
-    
-    function ( gltf ) {
+        mesh = gltf.scene;
+        mesh.scale.set( 10, 10, 10 );
+        scene.add( mesh );
+        
+		scene.add( gltf.scene );
 
-            var cube = new THREE.Mesh( geometry, material );
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Scene
+		gltf.scenes; // Array<THREE.Scene>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
 
-            mesh = gltf.scene;
-            
-            mesh.scale.set( 10, 10, 10 );
-            scene.add( mesh );
+	},
+	// called while loading is progressing
+	function ( xhr ) {
 
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
-            // gltf.traverse( function( child ) {
-            //     if ( child instanceof THREE.Mesh ) {
-            //         child.material = ship_material;
-            //     }
-            // } );
-            // scene.add( gltf );
+	},
+	// called when loading has errors
+	function ( error ) {
 
-            //scene.add( gltf.scene );
+		console.log( 'An error happened' );
 
-            // gltf.animations; // Array<THREE.AnimationClip>
-            // gltf.scene; // THREE.Scene
-            // gltf.scenes; // Array<THREE.Scene>
-            // gltf.cameras; // Array<THREE.Camera>
-            // gltf.asset; // Object
-
-    },
-    // called when loading is in progresses
-    function ( xhr ) {
-
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-    },
-    // called when loading has errors
-    function ( error ) {
-
-            console.log( 'An error happened' );
-
-    }
+	}
 );
+
+// LIGHTS
+
+var ambientLight = new THREE.AmbientLight( 0x404040, 50.0);
+scene.add(ambientLight);
+
+// var light = new THREE.PointLight(0xffffff,10.0,150);
+// light.position.set( 50,50, 100 );
+// scene.add(light);
+
+var light = new THREE.DirectionalLight( 0xffffff, 20, 150 );
+light.target = scene;
+scene.add( light );
+
+// var spotLight = new THREE.SpotLight( 0xffffff );
+// spotLight.position.set( 100, 1000, 100 );
+// scene.add( spotLight );
+
+// var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+// scene.add( light );
+
 
 function render() {
     requestAnimationFrame( render ); 
@@ -87,6 +107,21 @@ function render() {
 }
 
 render();
+
+const init = () => {
+	window.addEventListener(`resize`, onWindowResize, false);
+	onWindowResize();
+};
+const onWindowResize = () => {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	// uniforms.iResolution.value.x = window.innerWidth;
+	// uniforms.iResolution.value.y = window.innerHeight;
+	renderer.setSize(window.innerWidth, window.innerHeight);
+};
+
+
+init();
 
 
 
